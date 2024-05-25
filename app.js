01da5bfd4195e1fea17993da001386b8d111d7eb
shehnila-wahab <160872560+shehnila-wahab@users.noreply.github.com>
2024-05-25 23:05:08 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var main_1 = require("../class 15/main");
+console.log("PRINITING NUMBER OF GUEST INVITED");
+console.log("We had finally invited".concat(main_1.guest_list.length, ",\"from exercise 14"));
---
+++ app.ts
@@ -0,0 +1,3 @@
+import{ guest_list } from '../class 15/main'
+console.log("PRINITING NUMBER OF GUEST INVITED")
+console.log(`We had finally invited${guest_list.length},"from exercise 14`);
